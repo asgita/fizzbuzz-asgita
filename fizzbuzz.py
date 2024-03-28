405178e4ea071166ea48c6813977dfd1ff28ea11
gita <asgita1332@gmail.com>
2024-03-27 20:15:19 -0400

    print numbers

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
+# FizzBuzz printing numbers 1 to 101
 
+for num in range (101):
+   #  print(num)
+
+ # number is dividable by 3 print fizz
+     
+     if num %3==0:
+        print("fizz")
+     else:
+        print(num)    
