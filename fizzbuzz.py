f276b9fd87fe1938225a83a3e8bba3a24d59b52b
gita <asgita1332@gmail.com>
2024-03-27 22:10:22 -0400

    print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,18 @@
 for num in range (101):
    #  print(num)
 
- # number is dividable by 3 print fizz
+ # number is dividable by 3 print Fizz, number is dividable by 5 print buzz, number is dividable by 3 and 5
      
-     if num %3==0:
-        print("fizz")
-     else:
-        print(num)    
+     if    num % 3==0 and num % 5==0:
+           print("fizzbuzz")
+           continue
+     elif  num % 3==0: 
+           print("fizz")
+           continue
+     elif  num % 5==0: 
+           print("buzz")
+           continue
+     print(num)
+                
+
+                 
